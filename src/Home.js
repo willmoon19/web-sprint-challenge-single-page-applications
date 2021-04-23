1d817f936f0e159d11afce02ea01288636025a0a
William Moon <william-moon@lambdastudents.com>
2021-04-23 16:59:17 -0700

    message

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 
 export default function Homepage() {
-    const history = useHistory()
-    const routeToOrder = () => {
-        history.push("/")
-    }
+    // const history = useHistory()
+    // const routeToOrder = () => {
+    //     history.push("/")
+    // }
 
     return (
         <div>
             <h1>Home Page</h1>
+            <p>This is a bit of a failure on my part. I had very little time, I am sick after getting the covid vaccine and the orientation took quite awhile. I would have liked another hour or so to properly tidy things up and add some style</p>
             <img className="homeImage" src="../Assets/pizza.jpg" alt="" />
         </div>
     )
