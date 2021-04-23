a19332f01f5354bc9ad67bafe069a9c2de0b34d5
William Moon <william-moon@lambdastudents.com>
2021-04-23 11:43:18 -0700

    pages built, pizza inputs made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Route, Switch } from "react-router-dom";
+
+import PizzaOrder from "./Pizza"
+import Homepage from "./Home"
+
+const defaultPlaceInfo = {
+  name: "",
+  cost: "",
+  style: "",
+  foodType: "",
+  time: "",
+  deliveryFee: "",
+}
 
 const App = () => {
+  const [place, setPlace] = useState([]);
+  const [placeInfo, setPlaceInfo] = useState(defaultPlaceInfo);
+
+  const updateInfo = (newName, newValue) => {
+
+  }
+
+  const submitInfo = () => {
+
+  }
+  
+
+
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Switch>
+        <Route path="/pizza">
+          <PizzaOrder info={placeInfo} update={updateInfo} submit={submitInfo} />
+        </Route>
+        <Route path="/">
+          <Homepage info={placeInfo} />
+        </Route>
+      </Switch>
+      
     </>
   );
 };
---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function Homepage(props){
+
+
+
+
+
+
+
+
+
+<div className="container">
+<img />
+<h2>Are you Craving...</h2>
+<form onClick={}>
+  <button>Pizza?</button>
+</form>
+</div>
